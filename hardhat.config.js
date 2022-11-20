cce31ee2c1466a81e81a97a08a04ca631d040d6a
awongh <akira@awongh.com>
2022-11-19 18:20:31 -0800

    change around a few things

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,11 @@
-/** @type import('hardhat/config').HardhatUserConfig */
+const dotenv = require('dotenv')
+const path = require('path');
+const ThirdwebSDK = require('@thirdweb-dev/sdk');
+
+// Importing and configuring our .env file that we use to securely store our environment variables
+dotenv.config({ path: path.resolve(__dirname, './.env') });
+dotenv.config();
+
 module.exports = {
   solidity: "0.8.17",
 };
--- scripts/1-init.js
+++ scripts/1-init.js
@@ -1,12 +1,14 @@
 import { ThirdwebSDK } from "@thirdweb-dev/sdk";
 
-// Importing and configuring our .env file that we use to securely store our environment variables
-import dotenv from "dotenv";
-dotenv.config();
+const runMain = () => {
+  try {
 
-const sdk = ThirdwebSDK.fromPrivateKey(
-  process.env.PRIVATE_KEY,
-  'http://127.0.0.1:8545'
-);
+    const sdk = ThirdwebSDK.fromPrivateKey( process.env.PRIVATE_KEY, 'http://127.0.0.1:8545');
+    console.log( sdk );
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
 
-
+runMain();
